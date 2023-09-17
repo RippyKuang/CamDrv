7ca55ae1db2ec7f0a4a778b61d64c0952c6c37be
Kuang <Tentpole@126.com>
2023-09-17 10:56:33 +0800

    new file:   CMakeLists.txt 	new file:   CamDrv.cpp 	new file:   config.h.in

---
+++ CamDrv.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.h"
+using namespace std;
+
+int main(int argc, char **argv) {
+	cout << "Version " << CamDrv_VERSION_MAJOR << "." << CamDrv_VERSION_MINOR << endl;
+	return 0;
+}
