ca135efb162f73a75d6b604c1e6b83b7357b0be4
Kuang <Tentpole@126.com>
2023-09-21 23:06:07 +0800

    new file:   dengtiao/18.jpg 	new file:   dengtiao/19.jpg 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -10,32 +10,53 @@
 
 int main() {
 	std::cout<<CamDrv_VERSION_MAJOR<<"."<<CamDrv_VERSION_MINOR<<std::endl;
-
-    cv::Mat src=cv::imread("/home/kuang/project/CamDrv/dengtiao/17.jpg");
-    int threhdGray = 142, threhdRed = 245,threWrite=240;
-    cv::Mat kernel1 = getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
-    cv::Mat kernel2 = getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
+    cv::Mat src=cv::imread("/home/kuang/project/CamDrv/dengtiao/0.jpg");
+    int threhdGray = 200, threhdRed = 245,threWrite=240;
+    cv::Mat kernel1 = getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+    cv::Mat grayimg,aftr_Graythd,aftr_Redthd,and_img,white_mask,imgDil,imgDil2,imgEro;
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
     std::vector<cv::Mat> Trichlimgs;
-    cv::Mat grayimg,aftr_Graythd,aftr_Redthd,and_img,white_mask;
     cv::namedWindow("Trackbars");
     cv::createTrackbar("threhdGray", "Trackbars", &threhdGray, 255);
     cv::createTrackbar("threhdRed", "Trackbars", &threhdRed, 255);
     cv::createTrackbar("threhdWrite", "Trackbars", &threWrite, 255);
-    cv::split(src, Trichlimgs);
-    cv::Mat rchlimg = Trichlimgs[2];
-    cv::cvtColor(src, grayimg, cv::COLOR_BGR2GRAY);
-    cv::imshow("src", src);
+    int cnt=0;
+
     while(true) {
+        int x=cv::waitKey(0);
+        if(x =='n'){
+            src=cv::imread("/home/kuang/project/CamDrv/dengtiao/"+std::to_string(++cnt)+".jpg");
+        }
+        cv::split(src, Trichlimgs);
+        cv::Mat rchlimg = Trichlimgs[2];
+        cv::cvtColor(src, grayimg, cv::COLOR_BGR2GRAY);
         cv::threshold(grayimg, aftr_Graythd, threhdGray, 255, cv::THRESH_BINARY);
         cv::threshold(rchlimg, aftr_Redthd, threhdRed, 255, cv::THRESH_BINARY);
         cv::threshold(grayimg, white_mask, threWrite, 255, cv::THRESH_BINARY_INV);
         cv::bitwise_and(aftr_Redthd, aftr_Graythd&white_mask, and_img);
-        cv::Mat imgDil, imgEro, imgDil2;
-        cv::dilate(and_img, imgDil, kernel1);
-        cv::dilate(imgDil, imgDil2, kernel1);
-        cv::erode(imgDil2, imgEro, kernel2);
-        cv::imshow("andimg", and_img);
-        cv::waitKey(10);
+        dilate(and_img, imgDil, kernel1);
+
+
+
+        cv::findContours(imgDil, contours, hierarchy, cv::RETR_EXTERNAL,cv::CHAIN_APPROX_NONE);
+        for(int x=0;x<contours.size();x++){
+            if(cv::contourArea(contours[x])>30) {
+                cv::RotatedRect ellipse = cv::fitEllipse(contours[x]);
+
+                if(ellipse.size.height/ellipse.size.width>3 &&( ellipse.angle<30 ||ellipse.angle>150)){
+                    cv::putText(src, std::to_string(ellipse.angle), ellipse.center, cv::FONT_HERSHEY_DUPLEX, 1,
+                            cv::Scalar(255, 0, 255), 2);
+                    cv::drawContours(src, contours, x, cv::Scalar(255, 0, 255), 3);
+                    cv::ellipse(src, ellipse, cv::Scalar(0,255,0), 1);
+
+                }
+            }
+        }
+
+        cv::imshow("and_img", and_img);
+        cv::imshow("src", src);
+
     }
 	return 0;
 }
